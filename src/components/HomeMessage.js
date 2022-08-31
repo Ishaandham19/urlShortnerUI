5f8a6a7f93b488d053b483e29994cc87083cf12b
Ishaan Dham <ishaandham01@gmail.com>
2022-08-30 17:45:25 -0700

    changed base url, added home message, styling stuff

--- src/components/HomeMessage.js
+++ src/components/HomeMessage.js
@@ -1,8 +1,13 @@
 const HomeMessage = () => {
     return (
-        <div>
-            Welcome to App! Login in to start.
-        </div>
+        <>
+            <div id="welcome-msg" className="center-div gradient-text">
+                <h1>Welcome! Create your custom short urls</h1>
+            </div>
+            <div className="center-div gradient-text">
+                <h3>Login to start!</h3>
+            </div>
+        </>
     )
 }
 
